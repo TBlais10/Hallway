3781b6cd1b3215d60e4308fc8a3a7a1bb2d68b88
TBlais10 <blaislighting@gmail.com>
2022-05-13 11:39:24 -0400

    Added method in Application class that automatically loads ENums into mySQL database. Added methods into Role Repository and removed unused imports

--- src/main/java/com/careerdevs/myHalwayLocker/repositories/RoleRepository.java
+++ src/main/java/com/careerdevs/myHalwayLocker/repositories/RoleRepository.java
@@ -3,9 +3,20 @@ package com.careerdevs.myHalwayLocker.repositories;
 import com.careerdevs.myHalwayLocker.Auth.ERole;
 import com.careerdevs.myHalwayLocker.Auth.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(ERole name);
+
+    @Query(value = "select count(*) from role",
+            nativeQuery = true)
+    int checkTableSize();
+
+    @Query(value = "Insert into role (name) name values(:roleName) ",
+            nativeQuery = true)
+    Set<ERole> insertRoles(@Param("roleName")ERole role);
 }
--- src/main/java/com/careerdevs/myHalwayLocker/repositories/UserRepository.java
+++ src/main/java/com/careerdevs/myHalwayLocker/repositories/UserRepository.java
@@ -3,7 +3,6 @@ package com.careerdevs.myHalwayLocker.repositories;
 import com.careerdevs.myHalwayLocker.Auth.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
